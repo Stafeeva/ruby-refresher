3e0efac00d259fa804e8cf0e7d3062f97bd9b279
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-04-03 11:26:13 +0100

    Completed 2 challenges

--- lib/questions.rb
+++ lib/questions.rb
@@ -49,11 +49,18 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  if string.length % 2 == 0
+    string.slice(0, (string.length / 2))
+  else
+    string.slice(0, (string.length / 2) + 1)
+  end
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  number = 0 - number if number > 0
+  number if number < 0
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
